51097bbb6ea70bca030f669faea654d04606cbc1
S.Umar <shumarb@outlook.com>
2024-06-12 21:13:40 +0800

    Removed fxconvert subfolder

--- projects/fxconvert/src/main/java/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-/**
- * UserDoesNotExistException is an exception where a user does not exist 
- */
-package exceptions;
-
-public class UserNotFoundException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-}
